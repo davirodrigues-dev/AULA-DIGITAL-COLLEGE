df31fd052066473c450f327e7e66fd5e086f5605
Davi <davi495330@gmail.com>
2026-06-01 21:54:48 -0300

    botao modo escuro

---
+++ 2 - JAVASCRIPT/AULA 7/main.js
@@ -0,0 +1,20 @@
+const botaoModoEscuro = document.getElementById('btn');
+const body = document.body;
+const titulo = document.querySelector('h1');
+const paragrafo = document.getElementById('text');
+
+if (botaoModoEscuro) {
+    botaoModoEscuro.addEventListener('click', () => {
+        body.classList.toggle('dark-mode');
+
+        const darkMode = body.classList.contains('dark-mode');
+
+        botaoModoEscuro.setAttribute('aria-label', darkMode ? 'Ativar modo claro': 'Ativar modo escuro');
+
+        titulo.textContent = darkMode ? 'Ativar Modo Claro' : 'Ativar Modo Escuro';
+        paragrafo.textContent = darkMode
+            ? 'O modo escuro está ativo agora.'
+            : 'O modo claro está ativo agora.';
+        botaoModoEscuro.textContent = darkMode ? 'Ativar Modo Claro' : 'Ativar Modo Escuro';
+    });
+}
