e3a332830acb56f4e6948c7546b2cff4dbe9c598
Davi <davi495330@gmail.com>
2026-06-08 18:58:58 -0300

    .

--- 2 - JAVASCRIPT/AULA 7/main.js
+++ 2 - JAVASCRIPT/AULA 7/main.js
@@ -1,20 +1,25 @@
-const botaoModoEscuro = document.getElementById('btn');
+const botaoModoEscuro = document.getElementById("btn");
 const body = document.body;
-const titulo = document.querySelector('h1');
-const paragrafo = document.getElementById('text');
+const titulo = document.querySelector("h1");
+const paragrafo = document.getElementById("text");
 
 if (botaoModoEscuro) {
-    botaoModoEscuro.addEventListener('click', () => {
-        body.classList.toggle('dark-mode');
+  botaoModoEscuro.addEventListener("click", () => {
+    body.classList.toggle("dark-mode");
 
-        const darkMode = body.classList.contains('dark-mode');
+    const darkMode = body.classList.contains("dark-mode");
 
-        botaoModoEscuro.setAttribute('aria-label', darkMode ? 'Ativar modo claro': 'Ativar modo escuro');
+    botaoModoEscuro.setAttribute(
+      "aria-label",
+      darkMode ? "Ativar modo claro" : "Ativar modo escuro"
+    );
 
-        titulo.textContent = darkMode ? 'Ativar Modo Claro' : 'Ativar Modo Escuro';
-        paragrafo.textContent = darkMode
-            ? 'O modo escuro está ativo agora.'
-            : 'O modo claro está ativo agora.';
-        botaoModoEscuro.textContent = darkMode ? 'Ativar Modo Claro' : 'Ativar Modo Escuro';
-    });
+    titulo.textContent = darkMode ? "Ativar Modo Claro" : "Ativar Modo Escuro";
+    paragrafo.textContent = darkMode
+      ? "O modo escuro está ativo agora."
+      : "O modo claro está ativo agora.";
+    botaoModoEscuro.textContent = darkMode
+      ? "Ativar Modo Claro"
+      : "Ativar Modo Escuro";
+  });
 }
